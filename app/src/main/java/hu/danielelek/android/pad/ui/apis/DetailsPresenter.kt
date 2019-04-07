793dad749a5fa25dcfc50fca2cb15a2ba624db89
Daniel Elek <elekdani95@gmail.com>
2019-04-07 19:25:40 +0200

    Add and details fragment skeleton created

---
+++ app/src/main/java/hu/danielelek/android/pad/ui/apis/DetailsPresenter.kt
@@ -0,0 +1,10 @@
+package hu.danielelek.android.pad.ui.apis
+
+import hu.danielelek.android.pad.model.Api
+import hu.danielelek.android.pad.ui.Presenter
+
+class DetailsPresenter: Presenter<DetailsScreen>() {
+    fun saveChanges(api: Api) {
+        TODO("not implemented")
+    }
+}
