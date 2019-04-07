28b5cc4d209cc0c501556e6699ab8f16b0b4aa94
Daniel Elek <elekdani95@gmail.com>
2019-04-07 19:26:00 +0200

    AboutActivity skeleton created

---
+++ app/src/main/java/hu/danielelek/android/pad/ui/about/AboutActivity.kt
@@ -0,0 +1,13 @@
+package hu.danielelek.android.pad.ui.about
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import hu.danielelek.android.pad.R
+
+class AboutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+}
